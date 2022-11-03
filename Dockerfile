a38d33cef852b5f8189aaa83e79b367d56856e99
Nahuel Spiguelman <nahuel.spiguelman1@hotmail.com>
2022-11-03 11:29:06 -0300

    Agrego logger en todos los endpoints, nodemon, y el bloqueo del user

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
 FROM node:16
 WORKDIR /usr/src/app
 COPY package*.json ./
+
 RUN npm install
+RUN npm i -g nodemon
+
 COPY . .
 EXPOSE 5002
-CMD [ "node", "server.js" ]
+ENTRYPOINT ["npm", "start"]
--- src/routes/SessionRoutes.js
+++ src/routes/SessionRoutes.js
@@ -1,5 +1,6 @@
 module.exports = app => {
   const session = require('../controller/SessionController');
+  const logger = require('../../winston');
   const router = require('express').Router();
 
   const handlerResponse = (req, res) => {
@@ -7,9 +8,15 @@ module.exports = app => {
     res.status(statusCode).send(otherFields);
   };
 
+  const logInput = (req, res, next) => {
+    logger.info(JSON.stringify(req.query, undefined, 2));
+    logger.info(JSON.stringify(req.body, undefined, 2));
+    next();
+  };
+
   app.use('/', router);
-  router.post('/login', session.login, handlerResponse);
-  router.post('/login/send_token', session.sendToken, handlerResponse);
-  router.post('/token', session.authentication, handlerResponse);
-  router.post('/logout', session.logout, handlerResponse);
+  router.post('/login', logInput, session.login, handlerResponse);
+  router.post('/login/send_token', logInput, session.sendToken, handlerResponse);
+  router.post('/token', logInput, session.authentication, handlerResponse);
+  router.post('/logout', logInput, session.logout, handlerResponse);
 };
